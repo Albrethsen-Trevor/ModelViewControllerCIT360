48cee0ebacf1a212df46e13afd1ad0d88dc104c0
trevoralbrethsen <trevoralbrethsen@10.0.0.16>
2016-04-05 21:11:39 -0700

    Model View Controller (MVC) for CIT 360

---
+++ src/CalcMvc/CalculatorModel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CalcMvc;
+
+/**
+ *
+ * @author trevoralbrethsen
+ */
+public class CalculatorModel {
+    
+    private int calculationValue;
+    
+    public void addTwoNumbers(int firstNumber, int secondNumber){ 
+        
+        calculationValue = firstNumber + secondNumber;
+    
+    } public int getCalculationValue(){
+        
+        return calculationValue; }
+    
+}
